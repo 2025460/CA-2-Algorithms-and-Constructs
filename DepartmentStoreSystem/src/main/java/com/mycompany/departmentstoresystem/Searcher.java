e01a0846d09cdf66fc012155906932b7b12430da
Emill2107 <em.cavada@gmail.com>
2026-05-21 16:38:37 +0100

    Implement Search Algorithm

--- DepartmentStoreSystem/src/main/java/com/mycompany/departmentstoresystem/Searcher.java
+++ DepartmentStoreSystem/src/main/java/com/mycompany/departmentstoresystem/Searcher.java
@@ -3,11 +3,26 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.departmentstoresystem;
-
+import java.util.List;
 /**
  *
  * @author emcav
  */
 public class Searcher {
-    // Recursive sorting algorithm will be added commit 8
+
+    public static Employee recursiveSearch(List<Employee> list, String name) {
+        return search(list, name, 0);
+    }
+
+    private static Employee search(List<Employee> list, String name, int index) {
+
+        if (index >= list.size()) return null;
+
+        if (list.get(index).getName().equalsIgnoreCase(name)) {
+            return list.get(index);
+        }
+
+        return search(list, name, index + 1);
+    }
 }
+
